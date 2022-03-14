c8863acf98a0ad30e75f0846303e4b07dd37e5e1
AnaeleJoshua <anaelejoshua@gmail.com>
2022-03-14 12:08:16 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-/*program to print the size of types
- * float variable floata
- * char variable chart
- * int variable inte
- * double variable doubla
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-char charType;
-int inteType;
-float floatType;
-double doubleType;
-printf("size of char is:  %zu bytes\n", sizeof(charType));
-printf("size of int is: %zu bytes\n", sizeof(inteType));
-printf("size of float is: %zu bytes\n", sizeof(floatType));
-printf("size of double is: %zu bytes\n", sizeof(doubleType));
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+double g;
+printf("size of char is:  %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of int is: %lu bytes(s)\n", (unsigned of)sizeof(a));
+printf("size of a long int is: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("sixe of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of float is: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 
 
