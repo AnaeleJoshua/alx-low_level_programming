a86c2bf54d55078e727749fe3098cbe2bc5982a2
AnaeleJoshua <anaelejoshua@gmail.com>
2022-09-16 12:51:39 -0700

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int n);
-int largest_number(int var1, int var2, int var3)
+int largest_number(int var1, int var2, int var3);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
 
 #endif
