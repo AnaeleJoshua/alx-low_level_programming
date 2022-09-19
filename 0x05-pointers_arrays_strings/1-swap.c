0d639bedef1fb58d63b642ce629891054c51ce8b
AnaeleJoshua <anaelejoshua@gmail.com>
2022-09-19 14:37:01 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: pointer to interger
+ * @b: pointer ti integer
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c, d;
+	
+	c = *a;
+	d = *b;
+	*a = d;
+	*b = c;
+}
