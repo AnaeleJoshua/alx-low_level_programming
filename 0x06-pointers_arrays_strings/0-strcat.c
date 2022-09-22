4a39f8cb34804be7eda8b9802bf076b4addc0233
AnaeleJoshua <anaelejoshua@gmail.com>
2022-09-21 20:10:14 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
-int strcon();
-
-int main(void)
+/**
+ * _strcat- check the code
+ *
+ * @dest: destination string
+ * @src: source string
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)*
 {
-    strcon();
+int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);	
 }
-/*int strcon(void)
-   {
-        char s1[98] = "Hello ";
-        char s2[] = "World!\n";
-        char *ptr;
-        printf("%s\n", s1);
-        printf("%s",s2);
-        ptr = strcat(s1,s2);
-        printf("%s", s1);
-        printf("%s", s2);
-        printf("%s", ptr);
-
-   }*/
-
-
-        
-
