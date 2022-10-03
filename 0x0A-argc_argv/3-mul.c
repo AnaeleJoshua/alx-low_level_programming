663fa41db9464d6c3011da1232627b83e9e2fb69
AnaeleJoshua <anaelejoshua@gmail.com>
2022-10-03 05:36:33 -0700

    update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *  main -> program to multiply two numbers
+ *  @argc: argc parameter
+ *  @argv: an array of a command listed
+ *  Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	for (i = 1; i <= argc - 1; i++)
+	{
+		if (argc < 3)
+		{
+			printf("Error");
+		}
+		else
+		{
+			mul *= atoi(argv[i]);
+		}
+	}
+	return (0);
+}
