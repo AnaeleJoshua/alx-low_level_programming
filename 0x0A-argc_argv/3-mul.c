4e68c5e6e307197cb71448db6c6c52daaab30f4b
AnaeleJoshua <anaelejoshua@gmail.com>
2022-10-03 06:09:15 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,21 +8,20 @@
  *  @argv: an array of a command listed
  *  Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	int i;
 	int mul = 1;
 
+	if (argc != 3)
+                {
+                        printf("%s\n", "Error");
+			return (1);
+                }
+
 	for (i = 1; i <= argc - 1; i++)
 	{
-		if (argc < 3)
-		{
-			printf("Error");
-		}
-		else
-		{
 			mul *= atoi(argv[i]);
-		}
 	}
 	printf("%d\n", mul);
 	return (0);
